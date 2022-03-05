8b76f793cb063dd101babf8d465cd8e258085e7b
Vladislav Nekrasov <vladislav.nekrasov123@gmail.com>
2022-03-05 14:15:51 +0300

    Add project files.

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Practicing
+{
+    class Program
+    {
+        static void Main()
+        {
+            double first = 0;
+            double second = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("First number: ");
+                    first = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Second number: ");
+                    second = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"The divison result is {first / second}!");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message + "\nTry again! (Press enter to continue...)");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
